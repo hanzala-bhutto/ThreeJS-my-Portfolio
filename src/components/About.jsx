84982664963506b7acf5d347a0a144d6034e751b
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-04-23 14:40:21 +0500

    About section structured layed down

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React from 'react';
+import {motion} from 'framer-motion';
+import {Tilt} from 'react-tilt';
+import { styles } from '../styles';
+import {services} from '../constants';
+import {fadeIn, textVariant} from '../utils/motion';
+
+const ServiceCard = (({index, title, icon}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right","spring",0.5*index,0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}        
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title}
+            className='w-[100px] h-[100px] object-contain' 
+          />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+})
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("","",0.1,1)}
+        className='mt-4 text-secondary text-[17px] leading-[30px]'
+      >
+        I am a full stack developer with a passion for creating beautiful and functional web applications. I have a strong background in computer science and mathematics, and I am always looking for new ways to improve my skills and learn new technologies. I am currently working as a software engineer at <a href="https://www.3dlook.com/" target="_blank" rel="noreferrer">3DLOOK</a>, where I am responsible for developing and maintaining the company's web applications.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
