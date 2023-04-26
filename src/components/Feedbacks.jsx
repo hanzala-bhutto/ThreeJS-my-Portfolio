19f8e1b9089002d9e5379181a000775bc6411531
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-04-26 20:13:23 +0500

    Feedbacks Section Completed

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import {styles} from '../styles';
+import {SectionWrapper} from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+import {testimonials} from '../constants';
 
-const Feedbacks = () => {
+const FeedbackCard = (props) => {
   return (
-    <div>Feedbacks</div>
+    <motion.div
+      variants={fadeIn("","springb ",props.index*0.5,0.75)}
+      className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+    >
+      <p className='text-white font-black text-[48px]'>"</p>
+
+      <div className='mt-1'>
+        <p className='text-white tracking-wider text-[18px]'>{props.testimonial}</p>
+
+        <div className='mt-7 flex justify-between items-center gap-1'>
+          <div className='flex flex-1 flex-col'>
+            
+            <p className='text-white font-medium text-[16px]'>
+              <span className='blue-text-gradient'>@</span> {props.name}
+            </p>
+
+            <p className='mt-1 text-secondary text-[12px]'>
+              {props.designation} of {props.company}
+            </p>
+          
+          </div>
+
+          <img
+              src={props.image}
+              alt={props.name}
+              className='w-10 h-10 rounded object-cover'
+            />
+            
+        </div>
+      </div>
+
+    </motion.div>
   )
 }
 
-export default Feedbacks
+const Feedbacks = () => {
+  return (
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div className={`${styles.padding}
+      bg-tertiary rounded-2xl min-h-[300px]`}>
+
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What Others Say!</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SectionWrapper(Feedbacks,"");
